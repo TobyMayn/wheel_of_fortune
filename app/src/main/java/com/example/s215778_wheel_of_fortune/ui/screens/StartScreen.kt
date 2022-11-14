498ea26737cc8c9fc0e4db39976f25a4c9f90ec8
Tobias Mayn <tobiasmayn@gmail.com>
2022-11-14 12:53:45 +0100

    added material3 to project

---
+++ app/src/main/java/com/example/s215778_wheel_of_fortune/ui/screens/StartScreen.kt
@@ -0,0 +1,31 @@
+package com.example.s215778_wheel_of_fortune.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import com.example.s215778_wheel_of_fortune.R
+import androidx.compose.material3.CenterAlignedTopAppBar
+
+
+
+@Composable
+fun StartScreen (modifier: Modifier = Modifier) {
+
+}
+
+
+
+@Preview
+@Composable
+fun StartScreenPreview () {
+    StartScreen()
+}
